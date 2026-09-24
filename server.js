46e54ea78526db669b0423db74a6d97d3ba04c71
zoz <hazemmohamed112005@gmail.com>
2026-09-24 15:54:57 +0300

    Stage 2: public route and unverified protected route

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post("/signup", async (req, res, next) => {
   if (error) {
     return res.status(401).json({ error: error.message });
   }
-  return res.status(201).json({message:"created" , data});
+  return res.status(201).json({ message: "created", data });
 });
 app.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
@@ -44,12 +44,29 @@ app.post("/login", async (req, res, next) => {
     return res.status(400).json({ error: error.message });
   }
   if (!data.session) {
-    return res.status().json({message:"Invalid login credentials"})
+    return res.status(401).json({ message: "Invalid login credentials" });
   }
-  
+
   return res.status(201).json(data);
 });
 
+app.get("/public/info", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome stranger! This info is public.",
+  });
+});
+
+app.get("/profile", async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ error: "Access token required" });
+  }
+  const { data, error } = await supabase.auth.getUser(token);
+  if (error) {
+    return res.status(401).json({ error: error.message });
+  }
+  res.json({data})
+});
 app.listen(process.env.PORT, () => {
   console.log("server running");
 });
